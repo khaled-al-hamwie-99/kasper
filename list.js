7d49f5a537829d1bf5b54258b34d81208f8efbcd
khaled <sky455495@gmail.com>
2022-05-04 20:10:21 +0300

    adding the landing section

--- list.js
+++ list.js
@@ -1,14 +1,13 @@
 const btn = document.querySelector(".mobile-nav-toggle");
 const menu = document.getElementById("primary-navigation");
-console.log(btn.parentElement);
 btn.parentElement.addEventListener("click", menuNavigator);
-window.addEventListener("scroll", stopScrolling);
 
 function menuNavigator() {
     if (btn.getAttribute("aria-checked") == "false") {
         menu.style.display = "flex";
-        window.setTimeout(() => { menu.setAttribute("data-visible", "true") }, 0.0000000001)
+        window.setTimeout(() => { menu.setAttribute("data-visible", "true"); stopScrolling(); }, 0.0000000001)
         btn.setAttribute("aria-checked", "true");
+
     }
     else if (btn.getAttribute("aria-checked") == "true") {
         btn.setAttribute("aria-checked", "false");
@@ -17,7 +16,9 @@ function menuNavigator() {
     }
 }
 function stopScrolling() {
+    // console.log(menu.getAttribute("data-visible"));
     if (menu.getAttribute("data-visible") == "true") {
-        window.scrollTo(0, 0)
+        document.body.style.overflowY = "hidden";
+        window.scrollTo(0, 0);
     }
 }
